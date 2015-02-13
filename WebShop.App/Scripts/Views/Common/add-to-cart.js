5d5819a7c399fc2a4a225f2380d8a3d97f55de15
derwunderbar <derwunderbar@gmail.com>
2015-02-13 22:01:32 +0200

    Change article price and buy button vertical alignment to bottom

---
+++ WebShop.App/Scripts/Views/Common/add-to-cart.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('div.button > a').button().addClass('add-to-cart');
+});
+
+function addShoppingCartItem(shoppingCartUpdate) {
+    updateShoppingCartStatus(shoppingCartUpdate.TotalItems);
+    toastr.success('"' + shoppingCartUpdate.UpdatedItem.Title + '" ' + 'added to cart');
+}
--- WebShop.App/Scripts/Views/Common/catalog.js
+++ WebShop.App/Scripts/Views/Common/catalog.js
@@ -1,8 +1,44 @@
-﻿$(function() {
-    $('div.button > a').button().addClass('add-to-cart');
+﻿$(function () {
+    $('div.title').dotdotdot();
+    updateHeights();
 });
 
-function addShoppingCartItem(shoppingCartUpdate) {
-    updateShoppingCartStatus(shoppingCartUpdate.TotalItems);
-    toastr.success('"' + shoppingCartUpdate.UpdatedItem.Title + '" ' + 'added to cart');
+$(window).resize(function () {
+    updateHeights();
+});
+
+function updateHeights() {
+    var maxHeight = 0, rowY = 0, rowDivs = [], allDivs = $('.book-small'), count = allDivs.length;
+
+    allDivs.each(function (i) {
+        var div = $(this), y = div.offset().top, h = div.height();
+
+        if (h > maxHeight) maxHeight = h;
+        if (rowY == 0) rowY = y;
+
+        // Resize collected elements in case of new row
+        if (y > rowY) {
+            resizeElements(rowDivs, maxHeight);
+            rowDivs.length = 0;
+            maxHeight = h;
+        }
+
+        rowY = div.offset().top;
+        rowDivs.push(div);
+
+        // Resize collected elements in case of last row element
+        if (count - 1 == i) {
+            resizeElements(rowDivs, maxHeight);
+        }
+    });
+
+    // Update inner container layout
+    $('.book-small > div').addClass('relative-upd');
+    $('.book-small > div > div.bottom').addClass('bottom-upd');
+}
+
+function resizeElements(elements, height) {
+    for (var i = 0; i < elements.length; i++) {
+        $(elements[i]).height(height);
+    }
 }
